c19acb205f27ef457417d2353559a35aaa63cbf4
human97 <aleksey.n.popov@gmail.com>
2019-10-05 16:17:44 +0400

    add lessons-01

---
+++ lessons_01/js/script.js
@@ -0,0 +1,38 @@
+// #01
+console.log('Alex')
+
+
+// #02
+console.log(00)
+
+
+// #03
+console.log('Добро ' + 'пожаловать' + ' !')
+
+
+// #04
+//     2019 - 200
+
+
+// #05
+//alert(2019 - 200)
+
+
+// #06
+document.getElementById('one').textContent = 'Hello World'
+
+
+// #07
+document.getElementById('two').textContent = 12 * 12
+
+
+// #8
+document.querySelector('.one').textContent = 'Hello World'
+
+
+// #09
+document.querySelector('span').textContent = 'world'
+
+
+// #10
+document.querySelector('.three').innerHTML = '<h3>Hi</h3>'
