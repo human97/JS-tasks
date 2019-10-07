e6ca4426cc25c80645ecb440d8e0f4e4958dcaeb
human97 <aleksey.n.popov@gmail.com>
2019-10-07 14:04:45 +0400

    changed script.js in lessons-01

--- lessons_01/js/script.js
+++ lessons_01/js/script.js
@@ -1,34 +1,26 @@
 // #01
 console.log('Alex')
 
-
 // #02
 console.log(00)
-
 
 // #03
 console.log('Добро ' + 'пожаловать' + ' !')
 
-
 // #04
 //     2019 - 200
-
 
 // #05
 //alert(2019 - 200)
 
-
 // #06
 document.getElementById('one').textContent = 'Hello World'
-
 
 // #07
 document.getElementById('two').textContent = 12 * 12
 
-
 // #8
 document.querySelector('.one').textContent = 'Hello World'
-
 
 // #09
 document.querySelector('span').textContent = 'world'
@@ -37,45 +29,37 @@ document.querySelector('span').textContent = 'world'
 // #10
 document.querySelector('.three').innerHTML = '<h3>Hi</h3>'
 
-
 // #11
 let four = document.querySelector('.four')
 four.innerHTML = '<h4>Hello</h4>'
 four.innerHTML += '<p>world!</p>'
 
-
 // #12
 let a = document.querySelector('.five')
 a.innerHTML = 3.1415
 
-
 // #13
 let div7 = document.querySelector('.seven')
 div7.innerHTML = '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="pic">'
-
 
 // #14
 let z1 = 6
 let z2 = 3
 document.querySelector('.eight').innerHTML = z1 * z2
 
-
 // #15
 let y1 = 6
 let y2 = 3
 document.querySelector('.nine').innerHTML = y1 / y2
-
 
 // #16
 let x1 = 'Hello'
 x2 = 5
 document.querySelector('.ten').innerHTML = x1 + x2
 
-
 // #17
 let d1 = document.querySelector('.test-1')
 console.log(d1)
-
 
 // #18
 let d2 = document.querySelector('.test-2')
@@ -83,13 +67,11 @@ console.log(d2)
 d2 = 5
 console.log(d2)
 
-
 // #19
 let divS3=document.querySelector('.s3')
 console.log(divS3)
 divS3=document.querySelector('.s4')
 console.log(divS3)
-
 
 // #20
 // document.querySelector('body').innerHTML=''
