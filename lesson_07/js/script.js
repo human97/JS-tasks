587ba321824af7c0fa5a2608b708705df2860232
human97 <aleksey.n.popov@gmail.com>
2019-10-09 18:20:47 +0400

    add two solutions to task 02 block#07(script.js)

--- lesson_07/js/script.js
+++ lesson_07/js/script.js
@@ -4,3 +4,22 @@ function consoleName() {
 }
 
 document.querySelector('#btn01').onclick = consoleName
+
+// #02
+const btn02 = document.querySelector('#btn02')
+
+function consoleName02(name) {
+  console.log(name)
+}
+
+btn02.onclick = function () {
+  consoleName02('Sergey')
+}
+
+/* // тоже самое, стрелочная функция
+const consoleName002 = name => {
+  console.log(name)
+}
+
+btn02.onclick = () => consoleName002('Sergey')
+*/ 
