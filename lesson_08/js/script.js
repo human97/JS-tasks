3c64376a12278a766d34233c8b44fdaf8e3155b7
human97 <aleksey.n.popov@gmail.com>
2019-10-14 16:44:54 +0400

    add solution to task 02 block#08(script.js)

--- lesson_08/js/script.js
+++ lesson_08/js/script.js
@@ -12,3 +12,17 @@ function func_1() {
 }
 
 console.log(func_1())
+
+// #02
+let out2 = ''
+
+function func_2(num1, num2) {
+    let p1 = num1
+    while (p1 <= num2) {
+        out2 += `${p1} `
+        p1++
+    }
+    return out2
+}
+
+console.log(func_2(5, 14))
