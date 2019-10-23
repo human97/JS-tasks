300c302b931d1054a8869807578fa5d3fdcdd811
human97 <aleksey.n.popov@gmail.com>
2019-10-23 11:25:54 +0400

    add two solutions to task 01 block#09(script.js)

--- lesson_09/js/script.js
+++ lesson_09/js/script.js
@@ -1,1 +1,18 @@
 // #01
+const pU1 = document.querySelector('.u-1')
+const btn1 = document.querySelector('.btn1')
+
+function func_1() {
+    pU1.style.width = 200 + 'px'
+    pU1.style.height = 100 + 'px'
+}
+
+//btn1.onclick = func_1
+
+// вариант через стрелочную функцию
+const func_1s = () => {
+    pU1.style.width = 200 + 'px'
+    pU1.style.height = 100 + 'px'
+}
+
+btn1.onclick = func_1s
