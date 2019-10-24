63e29c978e4015815e7630bb5e478d5293230cb0
human97 <aleksey.n.popov@gmail.com>
2019-10-24 12:58:18 +0400

    add task 03 to block#09(html)

--- lesson_09/js/script.js
+++ lesson_09/js/script.js
@@ -3,16 +3,27 @@ const pU1 = document.querySelector('.u-1')
 const btn1 = document.querySelector('.btn1')
 
 function func_1() {
-    pU1.style.width = 200 + 'px'
-    pU1.style.height = 100 + 'px'
+    pU1.style.width = '200px'
+    pU1.style.height = '100px'
 }
 
 //btn1.onclick = func_1
 
 // вариант через стрелочную функцию
 const func_1s = () => {
-    pU1.style.width = 200 + 'px'
-    pU1.style.height = 100 + 'px'
+    pU1.style.width = '200px'
+    pU1.style.height = '100px'
 }
 
 btn1.onclick = func_1s
+
+
+// #02
+const btn2 = document.querySelector('.btn2')
+const pU2 = document.querySelector('.u-2')
+
+function func_2() {
+    pU2.classList.add('css-1')
+}
+
+btn2.onclick = func_2
