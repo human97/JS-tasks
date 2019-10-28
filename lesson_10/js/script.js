a926d4b4103224a7cd1bb1406955d4bb3faf7387
human97 <aleksey.n.popov@gmail.com>
2019-10-28 16:22:22 +0400

    add solution to task 02 block#10(script.js)

--- lesson_10/js/script.js
+++ lesson_10/js/script.js
@@ -1,3 +1,9 @@
 // #01
 const array1 = ['Man', 'Gary', 47, true]
 console.log(array1)
+
+
+// #02
+const array2 = ['Hello', 'wold', '!', 100, true, false, 'Best']
+const div2 = document.querySelector('.div2')
+div2.innerHTML = array2
