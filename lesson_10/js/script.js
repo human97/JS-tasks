84c5533e5b0fdc8a9be993486bbdddecdc0c329b
human97 <aleksey.n.popov@gmail.com>
2019-10-28 16:27:48 +0400

    add solution to task 03 block#10(script.js)

--- lesson_10/js/script.js
+++ lesson_10/js/script.js
@@ -7,3 +7,9 @@ console.log(array1)
 const array2 = ['Hello', 'wold', '!', 100, true, false, 'Best']
 const div2 = document.querySelector('.div2')
 div2.innerHTML = array2
+
+
+// #03
+const a = [2, 'hello', 17, 34, 'privet']
+const div3 = document.querySelector('.div3')
+div3.innerHTML = a.length
