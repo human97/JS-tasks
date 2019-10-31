0f68605eb9b2f01b2cd68f101ddfc8d8376a6bda
human97 <aleksey.n.popov@gmail.com>
2019-11-01 00:44:21 +0400

    add solution to task 01 block#11(script.js)

--- lesson_11/js/script.js
+++ lesson_11/js/script.js
@@ -1,1 +1,11 @@
 // #01
+let arr1 = []
+const inp1 = document.querySelector('.inp1')
+const btn1 = document.querySelector('.btn1')
+const div1 = document.querySelector('.div1')
+let inp1Val
+btn1.onclick = function () {
+    inp1Val = inp1.value
+    arr1.push(inp1Val)
+    div1.textContent = arr1
+}
