fcb1411b98e99f37952d904216449e16c7ccc988
human97 <aleksey.n.popov@gmail.com>
2019-11-10 14:48:21 +0400

    add solution to task 01 to block#12(script.js)

--- lesson_12/js/script.js
+++ lesson_12/js/script.js
@@ -1,1 +1,20 @@
 // #01
+const btn01 = document.querySelector('.btn01')
+const div01 = document.querySelector('.div01')
+let out01 = ''
+const a = [
+    [1, 2, 3],
+    ['a', 'b', 'c'],
+    [4, 5, 6],
+    ['d', 'e', 'f'],
+    [7, 8, 9],
+]
+
+for (let i = 0; i < a.length; i++) {
+    for (let k = 0; k < a[i].length; k++) {
+        out01 += `${a[i][k]}&nbsp;`
+    }
+    out01 += `<br>`
+}
+
+btn01.onclick = () => div01.innerHTML = out01
