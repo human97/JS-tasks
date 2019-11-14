804ea305b6d6fd3982856c6c4c0161ea8db56804
human97 <aleksey.n.popov@gmail.com>
2019-11-15 00:13:50 +0400

    add solution to task 01 to block#13(script.js)

--- lesson_13/js/script.js
+++ lesson_13/js/script.js
@@ -1,1 +1,21 @@
 // #01
+const div1 = document.querySelector('.div1')
+const btn1 = document.querySelector('.btn1')
+let out1 = ''
+
+arr1 = {
+    3: 'hello',
+    'one': 'hi'
+}
+
+btn1.onclick = () => {
+    if (div1.innerHTML === '') {
+        for (let key in arr1) {
+            out1 += `${key} = ${arr1[key]}<br>`
+        }
+
+        div1.innerHTML = `${out1}`
+    } else {
+        false
+    }
+}
