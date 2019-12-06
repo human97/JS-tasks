29319c340f8dcc3108d43d51187861df8fae0ad1
human97 <aleksey.n.popov@gmail.com>
2019-12-06 18:25:06 +0400

    add solution to task 02 to block#15

--- lesson_15/js/lesson_15.js
+++ lesson_15/js/lesson_15.js
@@ -7,3 +7,13 @@ set1.add('yes')
 set1.add('#')
 
 console.log(set1)
+
+
+// #02
+const inp2 = document.querySelector('.inp2')
+const btn2 = document.querySelector('.btn2')
+
+btn2.addEventListener('click', () => {
+    set1.add(inp2.value)
+    console.log(set1)
+})
