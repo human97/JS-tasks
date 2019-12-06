3f480ff141cfe10cc0d876a8d0f3dbe9c1f9d9e0
human97 <aleksey.n.popov@gmail.com>
2019-12-06 17:10:47 +0400

    add a folder called lesson_15

--- lesson_14/js/lesson_14.js
+++ lesson_14/js/lesson_14.js
@@ -1,1 +1,21 @@
 // #01
+fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=Samara,RU&appid=b898573b7c51bba91d9c7661ffff6b52'
+    )
+    
+    .then(function (resp) {
+        return resp.json()
+    })
+
+    .then(function (data) {
+        console.log(data)
+        document.querySelector('.wrapper--title').textContent = data.name
+        document.querySelector('.day_of_the_week').innerHTML = `${new Date(data.dt*1000)
+.getDay() === new Date().getDay() ? 'Now' : new Date(data.dt*1000).toLocaleString('en', {
+                weekday: "long"})}, ${new Date(data.dt*1000).toLocaleString('en', {
+                    month: "short",
+                    day: "numeric"
+                })}`
+        document.querySelector('.temp_day').innerHTML = `${Math.round(data.main.temp-273)}&deg`
+        document.querySelector('.weather-item-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}.png">`
+    })
