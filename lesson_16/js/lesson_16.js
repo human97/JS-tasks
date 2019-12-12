18753282d00a1a65756429fadf19126af1ec6a96
human97 <aleksey.n.popov@gmail.com>
2019-12-12 18:22:02 +0400

    add solution to task 01 to block#16

--- lesson_16/js/lesson_16.js
+++ lesson_16/js/lesson_16.js
@@ -1,1 +1,15 @@
 // #01
+const div1 = document.querySelector('.div1')
+const btn1 = document.querySelector('.btn1')
+
+const arr1 = [5, 7, 9, 11, 13, 15]
+let arr1_res = ''
+
+const t1 = () => {
+    for (let i = 0; i < arr1.length; i++) {
+        arr1_res += `${arr1[i]}&nbsp;`
+    }
+    div1.innerHTML = `${arr1_res}`
+}
+
+btn1.addEventListener('click', t1)
