784e6dbc5f8a6b09ebb23b4e1521a3574f35141e
human97 <aleksey.n.popov@gmail.com>
2019-12-12 18:33:47 +0400

    fix solution to task 01 to block#16

--- lesson_16/js/lesson_16.js
+++ lesson_16/js/lesson_16.js
@@ -3,9 +3,9 @@ const div1 = document.querySelector('.div1')
 const btn1 = document.querySelector('.btn1')
 
 const arr1 = [5, 7, 9, 11, 13, 15]
-let arr1_res = ''
 
 const t1 = () => {
+    let arr1_res = ''
     for (let i = 0; i < arr1.length; i++) {
         arr1_res += `${arr1[i]}&nbsp;`
     }
