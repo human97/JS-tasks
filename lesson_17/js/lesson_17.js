ce5141292f192f49cf6f9b88532160c7d76a61b0
human97 <aleksey.n.popov@gmail.com>
2019-12-18 03:35:41 +0400

    add solution to task 01 to block#17

--- lesson_17/js/lesson_17.js
+++ lesson_17/js/lesson_17.js
@@ -1,1 +1,9 @@
 // #01
+const btn1=document.querySelector('.btn1')
+const arr1 = [4, 5, 6, 7, 12, 34, 56, 78, 90, 11]
+const t1 = () => {
+    let newArr1 = arr1.map(item => item * 2)
+    console.log(newArr1)
+}
+
+btn1.addEventListener('click', t1)
