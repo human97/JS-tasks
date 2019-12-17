7cf16774d5867a353448606b2f59849f6a095d83
human97 <aleksey.n.popov@gmail.com>
2019-12-18 03:49:55 +0400

    add solution to task 02 to block#17

--- lesson_17/js/lesson_17.js
+++ lesson_17/js/lesson_17.js
@@ -1,5 +1,5 @@
 // #01
-const btn1=document.querySelector('.btn1')
+const btn1 = document.querySelector('.btn1')
 const arr1 = [4, 5, 6, 7, 12, 34, 56, 78, 90, 11]
 const t1 = () => {
     let newArr1 = arr1.map(item => item * 2)
@@ -7,3 +7,14 @@ const t1 = () => {
 }
 
 btn1.addEventListener('click', t1)
+
+
+// #02
+const btn2 = document.querySelector('.btn2')
+const arr2 = [2, 3, 4, 5, 10, 11, 12]
+const t2 = () => {
+    const newArr2 = arr2.map(item => item ** 2)
+    console.log(newArr2)
+}
+
+btn2.addEventListener('click', t2)
