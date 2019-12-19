43c09ef34715fff959178d44779881fb0156a95c
human97 <aleksey.n.popov@gmail.com>
2019-12-19 17:17:46 +0400

    add task 09 to block#17

--- lesson_11/js/lesson_11.js
+++ lesson_11/js/lesson_11.js
@@ -1,15 +1,3 @@
-// функция очищения input
-function inputClear() {
-    this.value = ''
-}
-
-let inputAll = document.querySelectorAll('input') // получил все input
-// цикл на все input при событии onclick вешает функцию 
-for (let i = 0; i < inputAll.length; i++) {
-    inputAll[i].onclick = inputClear
-}
-//*********
-
 // #01
 let arr1 = []
 const inp1 = document.querySelector('.inp1')
