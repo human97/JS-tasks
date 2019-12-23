c532840541988b26711eb040af3b1fe8ebe77205
human97 <aleksey.n.popov@gmail.com>
2019-12-23 16:55:44 +0400

    add solution to task 01 to block#19

--- lesson_19/js/lesson_19.js
+++ lesson_19/js/lesson_19.js
@@ -1,1 +1,9 @@
 // #01
+const task1 = document.querySelector('.div-1')
+const out1 = document.querySelector('.out-1')
+
+function t1 () {
+    out1.textContent = this.textContent
+}
+
+task1.addEventListener('click', t1)
