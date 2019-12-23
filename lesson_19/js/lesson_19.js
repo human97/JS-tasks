6cb0a0f29f51322ef2460d71dc7f65cf85a50123
human97 <aleksey.n.popov@gmail.com>
2019-12-23 19:00:34 +0400

    add solution to task 02 to block#19

--- lesson_19/js/lesson_19.js
+++ lesson_19/js/lesson_19.js
@@ -2,8 +2,21 @@
 const task1 = document.querySelector('.div-1')
 const out1 = document.querySelector('.out-1')
 
-function t1 () {
+function t1() {
     out1.textContent = this.textContent
 }
 
 task1.addEventListener('click', t1)
+
+
+// #02
+
+function t2(e) {
+    console.log(e.altKey)
+    out2.textContent = e.altKey
+    return e.altKey
+}
+
+const div2 = document.querySelector('.div-2')
+const out2 = document.querySelector('.out-2')
+div2.addEventListener('click', t2)
