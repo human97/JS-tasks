b59fc8bc1ddf2d982a21c8acaf9ec843c5e019b0
human97 <aleksey.n.popov@gmail.com>
2019-12-26 15:05:56 +0400

    add descriptions of all tasks to block#20

--- lesson_20/js/lesson_20.js
+++ lesson_20/js/lesson_20.js
@@ -1,1 +1,11 @@
 // #01
+const inp1 = document.querySelector('.i-1')
+const out1 = document.querySelector('.out-1')
+
+function t1(e) {
+    let symbol = e.key
+    out1.textContent += symbol
+    return symbol
+}
+
+inp1.onkeypress = t1
