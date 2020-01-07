82b1cfdfe1dd4ee16e9bb2d8b6f08ca805ba286d
human97 <aleksey.n.popov@gmail.com>
2020-01-07 21:28:31 +0400

    add solution to task 02 to block#21

--- lesson_21/js/lesson_21.js
+++ lesson_21/js/lesson_21.js
@@ -4,3 +4,11 @@ function t1() {
 }
 
 document.querySelector('.div-1').addEventListener('touchstart', t1)
+
+
+// #02
+function t2(event) {
+    document.querySelector('.out-2').textContent = event.touches.length
+}
+
+document.querySelector('.div-2').addEventListener('touchstart', t2)
