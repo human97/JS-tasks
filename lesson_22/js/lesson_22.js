d9626bbfaa3a44605bc4ba9aa0effa5cb44529b8
human97 <aleksey.n.popov@gmail.com>
2020-01-15 16:16:12 +0400

    add solution to task 02 to block#22

--- lesson_22/js/lesson_22.js
+++ lesson_22/js/lesson_22.js
@@ -7,3 +7,14 @@ div1.onclick = function () {
     counter1++
     out1.textContent = counter1
 }
+
+
+// #02
+const div2 = document.querySelector('.div-2')
+const out2 = document.querySelector('.out-2')
+let counter2 = 0
+
+div2.ondblclick = function () {
+    counter2++
+    out2.textContent = counter2
+}
